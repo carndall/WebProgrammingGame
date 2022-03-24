32e43b79f138a3a46d95b2df31e496f124b73947
ChihumeyaEE <meresiaeke@gmail.com>
2022-03-23 23:22:52 -0400

    Added Welcome/Sign-up/Login Pages

---
+++ login-submit.php
@@ -0,0 +1,30 @@
+<?php
+
+    if(isset($_GET['name'])){
+        $name = $_GET['name']; 
+        echo "<br>"; 
+
+    }
+    $user = $_GET['name']; 
+    $lines = file("users.txt"); 
+    foreach($lines as $line){
+        if(strpos($line, $user) !== false){  // gets the users details 
+           echo "Welcome " .$user; 
+        }
+       
+    }
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title> </title>
+        <link href="style.css" rel="stylesheet">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title> </title>
+        <link href="style.css" rel="stylesheet">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <b>Welcome!</b>
+        <p>
+            
+        </p>
+    </body>
+</html>
