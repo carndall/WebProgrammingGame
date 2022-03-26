9af935cbc918c153ae064efc213b77c3c4c05ce2
ChihumeyaEE <meresiaeke@gmail.com>
2022-03-26 11:27:43 -0400

    board game connects to welcome page

---
+++ game.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title> Homepage </title>
+        <link href="style.css" rel="stylesheet">
+        <meta charset="UTF-8">
+        <link href="style.css" text="stylesheet">
+    </head>
+    <body>
+        <h1> Jeopardy </h1>
+        <h2> <u> How to Play </u> </h2>
+        <h2> <li> Pick a Category  </li>
+            <li> Pick an Amount  </li>
+        </h2>
+        <div class="categories">
+        <table>
+            <tr>
+                <th style="background: blue; color:white"> Music </th>
+                <th style="background: blue; color:white"> Sports </th>
+                <th style="background: blue; color:white"> Misc </th>
+            </tr>
+            <tr>
+                <td style="background:blue"><a href="../questions/question0-music.html">$200</a></td>
+                <td style="background:blue"><a href="../questions/question0-sports.html">$200</a></td>
+                <td style="background:blue"><a href="../questions/question0-misc.html">$200</a></td>
+            </tr>
+            <tr>
+            <td style="background:blue"><a href="../questions/question1-music.html">$400</a></td>
+                <td style="background:blue"><a href="../questions/question1-sports.html">$400</a></td>
+                <td style="background:blue"><a href="../questions/question1-misc.html">$400</a></td>
+            </tr>
+            <tr>
+            <td style="background:blue"><a href="../questions/question2-music.html">$600</a></td>
+                <td style="background:blue"><a href="../questions/question2-sports.html">$600</a></td>
+                <td style="background:blue"><a href="../questions/question2-misc.html">$600</a></td>
+            </tr>
+            <tr>
+            <td style="background:blue"><a href="../questions/question3-music.html">$800</a></td>
+                <td style="background:blue"><a href="../questions/question3-sports.html">$800</a></td>
+                <td style="background:blue"><a href="../questions/question3-misc.html">$800</a></td>
+            </tr>
+        </table>
+        </div>
+    </body>
+
+</html>
