6d6ee7b5055a4a68b28425deae41d12c193c7bb7
Chihumeya Eresia-Eke <55638001+ChihumeyaEE@users.noreply.github.com>
2022-03-26 00:57:49 -0400

    added a board

---
+++ board.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title> Homepage </title>
+        <link href="style.css" rel="stylesheet">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1> Jeopardy </h1>
+        <h2> <u> How to Play </u> </h2>
+        <h2> <li> Pick a Category  </li>
+            <li> Pick an Amount  </li>
+        </h2>
+        <table>
+            <tr>
+                <th> Category 1 </th>
+                <th> Category 2 </th>
+                <th> Category 3 </th>
+            </tr>
+            <tr>
+                <td href="" > $200 </td>
+                <td href="" > $200 </td>
+                <td href="" > $200 </td>
+            </tr>
+            <tr>
+                <td href="" > $400 </td>
+                <td href="" > $400 </td>
+                <td href="" > $400 </td>
+            </tr>
+            <tr>
+                <td href="" > $600 </td>
+                <td href="" > $600 </td>
+                <td href="" > $600 </td>
+            </tr>
+            <tr>
+                <td href="" > $800 </td>
+                <td href="" > $800 </td>
+                <td href="" > $800 </td>
+            </tr>
+        </table>
+    </body>
+
+</html>
