1795d4e68235bf21f5957ef40571886076ee3573
carndall <57276900+carndall@users.noreply.github.com>
2022-03-26 20:37:08 -0400

    Add files via upload

---
+++ login-error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Error</title>
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+	<h1>Please sign up first</h1>
+	<br>
+	<a href="welcome.php">Click here to return to the front page</a>
+</body>
+</html>
